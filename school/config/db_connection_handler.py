4a1f6c752659b77a452f3c6825157292154c81f6
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-06-05 18:17:45 -0300

    creating db connection handler and base class for models

---
+++ school/config/db_connection_handler.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+class DBConnectionHandler:
+    '''Class to handle the connection with the database'''
+    def __init__(self):
+        self.__engine = create_engine('mysql+pymysql://root:password@localhost:3306/cinema')
+        self.__session = None
+
+    @property
+    def engine(self):
+        return self.__engine
+
+    @property
+    def session(self):
+        return self.__session
+
+    def __enter__(self):
+        Session = sessionmaker(bind=self.__engine)
+        self.__session = Session()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.__session.close()
+
