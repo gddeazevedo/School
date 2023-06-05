b3debd40f0d19c3faa92535ee54d308b5f45e536
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-06-05 18:06:40 -0300

    adding database url

--- alembic/env.py
+++ alembic/env.py
@@ -20,6 +20,9 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 target_metadata = None
 
+db_url = 'mysql+pymysql://root:password@localhost:3306/school'
+config.set_main_option('sqlalchemy.url', db_url)
+
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
