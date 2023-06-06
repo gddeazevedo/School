b9208591329a84d0a31bb420095e0460c30da233
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-06-06 16:57:43 -0300

    script to create the database

--- script.sql
+++ script.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS school;
 
 USE school;
 
-CREATE DATABASE IF NOT EXISTS setores (
+CREATE TABLE IF NOT EXISTS setores (
     cod_setor INT PRIMARY KEY,
     nome VARCHAR(50) NOT NULL
 );
@@ -20,4 +20,49 @@ CREATE TABLE IF NOT EXISTS cursos (
     cod_curso INT PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
     ano_inicio DATETIME NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS professores (
+    cpf VARCHAR(11) PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    telefone VARCHAR(11) NOT NULL,
+    endereco VARCHAR(100) NOT NULL,
+    data_contratacao DATETIME NOT NULL,
+    salario FLOAT NOT NULL,
+    ativo BOOLEAN NOT NULL,
+    cod_curso INT NOT NULL,
+    CONSTRAINT cod_curso_fk1 FOREIGN KEY (cod_curso) REFERENCES cursos(cod_curso)
+);
+
+CREATE TABLE IF NOT EXISTS alunos (
+    cpf VARCHAR(11) PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    telefone VARCHAR(11) NOT NULL,
+    endereco VARCHAR(100) NOT NULL,
+    ativo BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS disciplinas (
+    cod_disciplina INT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    cpf_professor VARCHAR(11) NOT NULL,
+    CONSTRAINT cpf_professor_fk FOREIGN KEY (cpf_professor) REFERENCES professores(cpf)
+);
+
+CREATE TABLE IF NOT EXISTS cursos_disciplinas (
+    cod_curso INT NOT NULL,
+    cod_disciplina INT NOT NULL,
+    CONSTRAINT cod_curso_fk2 FOREIGN KEY (cod_curso) REFERENCES cursos(cod_curso),
+    CONSTRAINT cod_disciplina_fk1 FOREIGN KEY (cod_disciplina) REFERENCES disciplinas(cod_disciplina),
+    PRIMARY KEY (cod_curso, cod_disciplina)
+);
+
+CREATE TABLE IF NOT EXISTS inscritos (
+    cod_disciplina INT NOT NULL,
+    cpf_aluno VARCHAR(11) NOT NULL,
+    nota FLOAT NOT NULL DEFAULT 0,
+    vez INT NOT NULL DEFAULT 1,
+    CONSTRAINT cod_disciplina_fk2 FOREIGN KEY (cod_disciplina) REFERENCES disciplinas(cod_disciplina),
+    CONSTRAINT cpf_aluno_fk FOREIGN KEY (cpf_aluno) REFERENCES alunos(cpf),
+    PRIMARY KEY (cod_disciplina, cpf_aluno)
 );
