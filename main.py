2469b9e7564b6dd077d3f2efa8baf21047392cb6
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-06-05 22:19:26 -0300

    adding genereate migration command

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ import os
 def main():
     commands = {
         'migrate up': 'alembic upgrade head',
-        'migrate down': 'alembic downgrade -1'
+        'migrate down': 'alembic downgrade -1',
+        'generate migration': 'alembic revision -m '
     }
     try:
         if len(sys.argv) > 2:
             command_key = f'{sys.argv[1]} {sys.argv[2]}'
             command = commands.get(command_key)
-            os.system(command)
+            if command_key[0] == 'g':
+                print(command + f'"{sys.argv[3]}"')
+                os.system(command + f'"{sys.argv[3]}"')
+            else:
+                os.system(command)
         else:
             print('Running CLI')
     except Exception as e:
