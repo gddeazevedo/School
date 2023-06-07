72b84d8d691987557dd37753cc0e25e7c861ebbd
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-06-07 00:23:37 -0300

    implementing alunos repository crud

--- school/repositories/alunos_repository.py
+++ school/repositories/alunos_repository.py
@@ -5,20 +5,49 @@ from ..models import Aluno
 class AlunosRepository:
     @staticmethod
     def select_all() -> list[Aluno]:
-        pass
+        with DBConnectionHandler() as db:
+            return db.session.query(Aluno).all()
 
     @staticmethod
     def select_by_cpf(cpf: str) -> Aluno | None:
-        pass
+        with DBConnectionHandler() as db:
+            try:
+                return db.session.query(Aluno).filter(Aluno.cpf == cpf).first()
+            except Exception as e:
+                print(e)
+                return None
 
     @staticmethod
     def insert(cpf: str, nome: str, telefone: str, endereco: str, ativo: bool) -> None:
-        pass
+        with DBConnectionHandler() as db:
+            try:
+                new_aluno = Aluno(cpf=cpf, nome=nome, telefone=telefone, endereco=endereco, ativo=ativo)
+                db.session.add(new_aluno)
+                db.session.commit()
+            except Exception as e:
+                print(e)
+                return None
     
     @staticmethod
     def update(cpf: str, **new_values) -> None:
-        pass
+         with DBConnectionHandler() as db:
+            try:
+                db.session.query(Aluno)\
+                    .filter(Aluno.cpf == cpf)\
+                    .update(new_values)
+                db.session.commit()
+            except Exception as e:
+                print(e)
+                return None
 
     @staticmethod
     def delete(cpf: str) -> None:
-        pass
+        with DBConnectionHandler() as db:
+            try:
+                db.session.query(Aluno)\
+                    .filter(Aluno.cpf == cpf)\
+                    .delete()
+                db.session.commit()
+            except Exception as e:
+                print(e)
+                return None
