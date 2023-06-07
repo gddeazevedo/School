019c778b92284e966da49b22c9d0bb4e7154dbad
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-06-07 00:01:32 -0300

    exporting models

--- school/models/__init__.py
+++ school/models/__init__.py
@@ -1,2 +1,6 @@
-from .setor import Setor
+from .aluno import Aluno
+from .curso import Curso
+from .disciplina import Disciplina
 from .funcionario import Funcionario
+from .professor import Professor
+from .setor import Setor
