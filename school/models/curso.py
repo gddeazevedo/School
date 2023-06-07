8756c57bf70d4dd361c173e8100b581e0f1c60b4
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-06-06 22:49:35 -0300

    creating curso model

---
+++ school/models/curso.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from ..config.base import Base
+
+
+class Curso(Base):
+    __tablename__ = 'cursos'
+
+    cod_curso = Column(Integer, primary_key=True)
+    nome = Column(String(length=50), nullable=False)
+    ano_inicio = Column(DateTime, nullable=False)
+
+    professores = relationship('Professor', back_populates='curso', lazy='joined')
+
+    def __repr__(self) -> str:
+        return f'Curso(cod_curso={self.cod_curso}, nome={self.nome}, ano_inicio={self.ano_inicio})'
