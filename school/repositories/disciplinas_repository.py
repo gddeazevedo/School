5c2fbf25dfa4d84b1ef341a648b10b5396118e9b
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-06-07 00:35:56 -0300

    implementing crud for disciplinas

--- school/repositories/disciplinas_repository.py
+++ school/repositories/disciplinas_repository.py
@@ -5,20 +5,53 @@ from ..models import Disciplina
 class DisciplinasRepository:
     @staticmethod
     def select_all() -> list[Disciplina]:
-        pass
+        with DBConnectionHandler() as db:
+            return db.session.query(Disciplina).all()
 
     @staticmethod
-    def select_by_cpf(cod_disciplina: int) -> Disciplina | None:
-        pass
+    def select_by_cod_disciplina(cod_disciplina: str) -> Disciplina | None:
+        with DBConnectionHandler() as db:
+            try:
+                return db.session.query(Disciplina).filter(Disciplina.cod_disciplina == cod_disciplina).first()
+            except Exception as e:
+                print(e)
+                return None
 
     @staticmethod
-    def insert(cod_disciplina: int, nome: str, cpf_professor: str) -> None:
-        pass
-    
+    def insert(nome: str, cpf_professor: str) -> None:
+        with DBConnectionHandler() as db:
+            try:
+                new_disciplina = Disciplina(
+                    cod_disciplina=None,
+                    nome=nome,
+                    cpf_professor=cpf_professor
+                )
+                db.session.add(new_disciplina)
+                db.session.commit()
+            except Exception as e:
+                print(e)
+                return None
+
     @staticmethod
     def update(cod_disciplina: int, **new_values) -> None:
-        pass
+         with DBConnectionHandler() as db:
+            try:
+                db.session.query(Disciplina)\
+                    .filter(Disciplina.cod_disciplina == cod_disciplina)\
+                    .update(new_values)
+                db.session.commit()
+            except Exception as e:
+                print(e)
+                return None
 
     @staticmethod
     def delete(cod_disciplina: int) -> None:
-        pass
+        with DBConnectionHandler() as db:
+            try:
+                db.session.query(Disciplina)\
+                    .filter(Disciplina.cod_disciplina == cod_disciplina)\
+                    .delete()
+                db.session.commit()
+            except Exception as e:
+                print(e)
+                return None
