d4339f32a1bf15839a7d8fa701a63cb4aa01359d
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-06-07 00:00:41 -0300

    implementing desciplina model

---
+++ school/models/disciplina.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from ..config.base import Base
+
+
+class Disciplina(Base):
+    __tablename__ = 'disciplinas'
+
+    cod_disciplina = Column(Integer, primary_key=True)
+    nome = Column(String(length=50), nullable=False)
+    cpf_professor = Column(String(length=11), ForeignKey('professores.cpf', ondelete='CASCADE'))
+
+    professor = relationship('Professor', back_populates='disciplinas', lazy='joined')
+
+    def __repr__(self) -> str:
+        return f'Disciplina(cod_disciplina={self.cod_disciplina}, nome={self.nome})'
