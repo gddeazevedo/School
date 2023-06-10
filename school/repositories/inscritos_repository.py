33e0bd811b0820e4e8c31fda1403e5b7361dc6da
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-06-10 14:39:00 -0300

    implementing inscritos repository

---
+++ school/repositories/inscritos_repository.py
@@ -0,0 +1,65 @@
+from ..config.db_connection_handler import DBConnectionHandler
+from ..models import Inscrito
+
+
+class InscritosRepository:
+    @staticmethod
+    def select_all() -> list[Inscrito]:
+        with DBConnectionHandler() as db:
+            return db.session.query(Inscrito).all()
+
+    @staticmethod
+    def select_by_cpf_aluno_and_cod_disciplina(cpf_aluno: str, cod_disciplina: int) -> Inscrito | None:
+        with DBConnectionHandler() as db:
+            try:
+                return db.session.query(Inscrito).filter(
+                    Inscrito.cod_disciplina == cod_disciplina and Inscrito.cpf_aluno == cpf_aluno
+                )
+            except Exception as e:
+                print(e)
+                return None
+
+    @staticmethod
+    def insert(cpf_aluno: str, cod_disciplina: int, nota: float, vez: int) -> bool:
+        with DBConnectionHandler() as db:
+            try:
+                new_inscrito = Inscrito(
+                    cpf_aluno=cpf_aluno,
+                    cod_disciplina=cod_disciplina,
+                    nota=nota,
+                    vez=vez
+                )
+                db.session.add(new_inscrito)
+                db.session.commit()
+                return True
+            except Exception as e:
+                print(e)
+                return False
+    
+    @staticmethod
+    def update(cpf_aluno: str, cod_disciplina: int, **new_values) -> bool:
+        with DBConnectionHandler() as db:
+            try:
+                db.session.query(Inscrito)\
+                    .filter(
+                        Inscrito.cod_disciplina == cod_disciplina and Inscrito.cpf_aluno == cpf_aluno
+                    )\
+                    .update(new_values)
+                db.session.commit()
+                return True
+            except Exception as e:
+                print(e)
+                return False
+
+    @staticmethod
+    def delete(cpf_aluno: str, cod_disciplina: int) -> bool:
+        with DBConnectionHandler() as db:
+            try:
+                db.session.query(Inscrito)\
+                    .filter(Inscrito.cod_disciplina == cod_disciplina and Inscrito.cpf_aluno == cpf_aluno)\
+                    .delete()
+                db.session.commit()
+                return True
+            except Exception as e:
+                print(e)
+                return False
