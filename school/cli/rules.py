bdb46da1b0513528e4d522b4daaae6cb0f2d16e5
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-06-10 22:20:04 -0300

    inserting funcionarios

--- school/cli/rules.py
+++ school/cli/rules.py
@@ -38,21 +38,45 @@ def insert_curso():
 
 
 def insert_funcionario():
-        while True:
-            print('Cadastrar funcionario')
-            cod_curso = int(input('Digite o codigo do curso: '))
-            nome = input('Digite o nome do curso: ')
-            ano_inicio = int(input('Digite o ano de início do curso: '))
+    while True:
+        print('Cadastrar funcionario')
+        cpf = input('Digite o CPF do funcionario: ')
 
-            if repos.CursosRepository.insert(cod_curso=cod_curso, nome=nome, ano_inicio=ano_inicio):
-                print('Curso cadastrado com sucesso!')
-                input('Aperte enter para sair!')
-                os.system('clear')
-                return
+        while len(cpf) != 11:
+            print('CPF inválido! Tente novamente')
+            cpf = input('Digite o CPF do funcionario: ')
+
+        nome = input('Digite o nome do funcionário: ')
+        endereco = input('Digite o endereço do funcionário: ')
+        salario = float(input('Digite o salário do funcionário: '))
+        
+        while True:
+            cod_setor = int(input('Digite o código do setor: '))
+
+            setor = repos.SetoresRepository.select_by_cod_setor(cod_setor)
+
+            if setor == None:
+                print('Setor inexistente! Por favor tente novamente!')
             else:
-                print('Erro ao cadastrar o curso! Tente novamente!')
-                input('Aperte enter para tentar de novo!')
-                os.system('clear')
+                break
+
+        inserted = repos.FuncionariosRepository.insert(
+            cpf=cpf,
+            nome=nome,
+            endereco=endereco,
+            salario=salario,
+            cod_setor=cod_setor
+        )
+
+        if inserted:
+            print('Funcionário cadastrado com sucesso!')
+            input('Aperte enter para sair!')
+            os.system('clear')
+            return
+        else:
+            print('Erro ao cadastrar o funcionário! Tente novamente!')
+            input('Aperte enter para tentar de novo!')
+            os.system('clear')
 
 
 def insert_professor():
