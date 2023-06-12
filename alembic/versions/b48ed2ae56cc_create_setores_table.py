91e7baec48b376c87759620c9f7f610abfaa23c6
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-06-12 15:20:56 -0300

    making field name not unique

--- alembic/versions/b48ed2ae56cc_create_setores_table.py
+++ alembic/versions/b48ed2ae56cc_create_setores_table.py
@@ -20,7 +20,7 @@ def upgrade() -> None:
     op.create_table(
         'setores',
         sa.Column('cod_setor', sa.Integer, primary_key=True),
-        sa.Column('nome', sa.String(length=50), nullable=False, unique=True)   
+        sa.Column('nome', sa.String(length=50), nullable=False, unique=False)   
     )
 
 
